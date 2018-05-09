cf1b2e8e8c2217d1bc251da9ad9d28d76b49b74a
Zamid Rzayev <zamid513@gmail.com>
2018-05-09 09:53:06 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp 				= require('gulp'),
+		sass			  = require('gulp-sass'),
+		browserSync = require('browser-sync');
+
+
+
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/**/*.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+
+
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+	gulp.watch('app/sass/**/*.sass', ['sass']);
+});
